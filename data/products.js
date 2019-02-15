e927a807666c82c214dec6463c7b615af0a3544a
Dom Clemmer <thisisdom4@gmail.com>
2019-02-15 15:21:10 -0400

    add all products

---
+++ data/products.js
@@ -0,0 +1,77 @@
+// {
+//   name: ,
+//   price: ,
+//   description: ,
+//   image:
+// },
+const products = [
+  {
+    name: 'Hot Cocoa w/ Whipped Cream',
+    price: 9.82,
+    description: 'Decadent imported cocoa beans imported from Colombia, ground fine, and mixed with water and honey. Cow milk or almond available upon request.',
+    image: require('../assets/imgs/products/food-photographer-jennifer-pallian-173715-unsplash.png')
+  },
+  {
+    name: 'Chocolate Chip Cookies (6ct)',
+    price: 8.40,
+    description: 'The cookies are to die for. Flour imported from Sudan; Chocolate imported from Colombia. These cookies taste good no matter where you are from.',
+    image: require('../assets/imgs/products/food-photographer-jennifer-pallian-306898-unsplash.png')
+  },
+  {
+    name: 'Espresso w/ Chocolate Syrup',
+    price: 10.99,
+    description: 'Espresso beans from Italy. Chocolate Syrup from Moscow. Need we say more?',
+    image: require('../assets/imgs/products/giancarlo-duarte-543025-unsplash.png')
+  },
+  {
+    name: 'Rasberries & Chocolate Covered Walnuts',
+    price: 15.99,
+    description: 'Fresh local rasberries paired with walnuts dipped in chocolate. Made in-house. Fruity and savory with a crunch.',
+    image: require('../assets/imgs/products/joanna-kosinska-673126-unsplash.png')
+  },
+  {
+    name: 'Chocolate Dipped Oreos (2ct)',
+    price: 6.32,
+    description: 'Oreos dipped in our house chocolate sauce. Once set, we crown with whipped cream and mocha powder.',
+    image: require('../assets/imgs/products/kristiana-pinne-1241110-unsplash.png')
+  },
+  {
+    name: 'Crunch Bar Topped w/ Strawberries (3ct)',
+    price: 10.20,
+    description: 'Homemade crunch bar, filled with strawberry creme, with a sliced strawberry on top.',
+    image: require('../assets/imgs/products/kristiana-pinne-1241127-unsplash.png')
+  },
+  {
+    name: 'Mocha Muffins (2ct)',
+    price: 7.26,
+    description: 'Mocha sourced from Morroco, made with love locally.',
+    image: require('../assets/imgs/products/nordwood-themes-213933-unsplash.png')
+  },
+  {
+    name: 'Chocolate Covered Walnut (8ct)',
+    price: 5.47,
+    description: 'Our house chocolate blend with walnuts souced from California.',
+    image: require('../assets/imgs/products/nordwood-themes-493493-unsplash.png')
+  },
+  {
+    name: 'M&Ms (1 bag)',
+    price: 0.99,
+    description: 'A classic.',
+    image: require('../assets/imgs/products/robert-anasch-786372-unsplash.png')
+  },
+  {
+    name: 'Molten Lava Chocolate Cake (2ct)',
+    price: 17.99,
+    description: "I don't think your ready for this jelly. Give this one poke and warm chocolate sauce will flow like Mount Vesuvius.",
+    image: require('../assets/imgs/products/taylor-kiser-373459-unsplash.png')
+  },
+  {
+    name: '12 Layer Chocolate Cake',
+    price: 30.00,
+    description: "Decadance, personfied. Meticulously crafed for you to enjoy. Topped with rasberries to add a fruity element to something to rich for most.",
+    image: require('../assets/imgs/products/taylor-kiser-373463-unsplash.png')
+  }
+]
+
+
+export default products
