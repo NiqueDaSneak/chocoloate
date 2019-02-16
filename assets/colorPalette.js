38a5e9d6e06125566e4cc963d0d42227299d843b
Dom Clemmer <thisisdom4@gmail.com>
2019-02-16 12:51:21 -0400

    add additional colors

--- assets/colorPalette.js
+++ assets/colorPalette.js
@@ -4,7 +4,9 @@ const colors = {
   third: '#B96848',
   fourth: '#8F4020',
   darkGrey: '#393939',
-  lightGrey: '#C5C5C5'
+  lightGrey: '#C5C5C5',
+  success: '#67BE58',
+  decline: '#CB2446'
 }
 
 export default colors
