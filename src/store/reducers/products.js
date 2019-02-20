a0748e514f18852f90b41eb8c6efc0842245d045
Dom Clemmer <thisisdom4@gmail.com>
2019-02-19 20:29:01 -0500

    change logic for adding items to cart

--- src/store/reducers/products.js
+++ src/store/reducers/products.js
@@ -2,37 +2,39 @@ import { SHOW_PRODUCT_DETAILS, DISMISS_PRODUCT_DETAILS } from '../actions/action
 
 const initialState = {
   price: 0.00,
+  cart: [],
   modalVisible: false,
   selectedProduct: {
     name: ''
-  },
-  cart: []
+  }
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-
     case SHOW_PRODUCT_DETAILS:
+    return {
+      ...state,
+      modalVisible: true,
+      selectedProduct: action.product
+    }
+
+    case DISMISS_PRODUCT_DETAILS:
+    if (action.payload.addToCart) {
+      const newCart = [...state.cart, action.payload.product]
       return {
         ...state,
-          modalVisible: true,
-          selectedProduct: action.product
+        modalVisible: false,
+        price: state.price + action.payload.product.price,
+        cart: newCart
       }
+    } else {
+      return {
+        ...state,
+        modalVisible: false,
+      }
+    }
 
-      case DISMISS_PRODUCT_DETAILS:
-        if (action.payload.addToCart) {
-          return {
-          ...state,
-          modalVisible: false,
-          price: state.price + action.payload.product.price,
-          cart: state.cart.push(action.payload.product)
-        }
-        } else {
-          return {
-          ...state,
-          modalVisible: false,
-        }
-        }
+
     default:
       return state
   }
