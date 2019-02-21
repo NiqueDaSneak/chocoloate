365c67f5950bd5c9b378a8f2be8c053c2452f8ca
Dom Clemmer <thisisdom4@gmail.com>
2019-02-20 19:17:13 -0500

    proper binding of FinalBillModal to redux store

--- src/UI/Modal/FinalBillModal.js
+++ src/UI/Modal/FinalBillModal.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react'
 import { Button, Text, View, Image, StyleSheet, TouchableOpacity, Modal } from 'react-native';
 import colors from '../../../assets/colorPalette'
+import { connect } from 'react-redux'
 
-const FinalBillModal = ( props ) => {
-  return (
-    <View>
-      <Modal
-        animationType="slide"
-        transparent={false}
-        visible={props.showing} />
-    </View>
-  )
+import { showProductDetails, dismissProductDetails } from '../../store/actions/index'
+
+class FinalBillModal extends Component {
+  render() {
+    return (
+      <View>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.props.showing}>
+          <Text>{this.props.cart[0].name}</Text>
+        </Modal>
+      </View>
+    )
+  }
 }
 
-export default FinalBillModal
+const mapStatetoProps = (state) => {
+  return {
+    price: state.products.price,
+    modalVisible: state.products.modalVisible,
+    selectedProduct: state.products.selectedProduct,
+    cart: state.products.cart
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    showProductDetails: (product) => dispatch(showProductDetails(product)),
+    dismissProductDetails: (payload) => dispatch(dismissProductDetails(payload))
+  }
+}
+
+export default connect(mapStatetoProps, mapDispatchToProps)(FinalBillModal)
